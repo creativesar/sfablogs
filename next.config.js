acd4a4f389f1f91dc48a5e533aecb4e26fe1f9ad
creativesar <uniqueluck68@gmail.com>
2025-01-01 20:36:49 +0000

    ok

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    NEXT_PUBLIC_SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+    NEXT_PUBLIC_SANITY_TOKEN: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+  },
+  // ...existing configuration...
+};
