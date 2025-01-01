7cf82184298e7fed78cff14631179949b8a15f4d
creativesar <uniqueluck68@gmail.com>
2025-01-01 20:58:56 +0000

    ok

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  images: {
+    domains: ['cdn.sanity.io'],
+  },
   env: {
     NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
